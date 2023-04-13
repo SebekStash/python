e31856a310955b38f37c5a8bd13a584f8419619f
SebekStash <sebekforgit@gmail.com>
2023-04-13 21:10:11 +0000

    [heapsort.py] Add heapSort function

--- heapsort.py
+++ heapsort.py
@@ -14,3 +14,15 @@ def maxHeapify(array, index, heapsize):
     if(largest != index):
         array[largest], array[index] = array[index], array[largest]
         maxHeapify(array, largest ,heapsize-1)
+
+# range(start, stop, step)
+def heapSort(array, heapsize):
+    for i in range(heapsize / 2 - 1, -1, -1):
+        maxHeapify(array, i, heapsize)
+
+    for i in range(heapsize, -1, -1):
+        array[0], array[i] = array[i], array[0]
+        maxHeapify(array, 0, i)
+    
+
+    
